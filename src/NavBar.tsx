939ceccfcc87bb9d63b4824c74b401323f620385
mazad2 <karen.tihhomirov@gmail.com>
2023-03-23 13:47:58 +0200

    added navbar design

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -2,26 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function NavBar() {
   return (
-    <div>
+    <div className="">
       <div className=' place-content-center gap-x-20 flex flex-row pt-10'>
         
-          <button className='w-40 h-32 border-2 rounded-lg hover:drop-shadow-lg hover:bg-gray-100 duration-75'>About me</button>
-        
-          <button className='shadow-[-8px_10px_0_rgb(0,0,0)] border-2 hover:shadow-[8px_-10px_0px_rgb(0,0,0)] bg-white ease-out hover:translate-y-1 transition-all rounded'>
-            12312
+          <button className='shadow-[-5px_10px_5px_rgb(9,5,67)] hover:shadow-[-2px_5px_5px_rgb(0,0,0)] hover:drop-shadow-xl hover:translate-y-1 ease-out border-2 border-blue-900 rounded-lg w-40 h-20 bg-blue-400 hover:bg-blue-700 duration-200 font-mono text-2xl antialiased'>
+            About me
           </button>
         
-          <button className=' w-40 h-32 border-2 rounded-lg hover:drop-shadow-lg'>Contact</button>
-          <button className='w-40 group h-32 border-2 rounded-lg hover:drop-shadow-lg'>
-            <img className="w-10 group-hover:translate-x-20 group-hover:drop-shadow-xl  duration-300 shine-" src='src\assets\next-arrow.svg' alt='arrow'></img>
+          <button className='shadow-[-5px_10px_5px_rgb(9,5,67)] hover:shadow-[-2px_5px_5px_rgb(0,0,0)] hover:drop-shadow-xl hover:translate-y-1 ease-out border-2 border-blue-900 rounded-lg w-40 h-20 bg-blue-400 hover:bg-blue-700 duration-200 font-mono text-2xl antialiased'>
+            Experience
+          </button>
+
+          <button className='shadow-[-5px_10px_5px_rgb(9,5,67)] hover:shadow-[-2px_5px_5px_rgb(0,0,0)] hover:drop-shadow-xl hover:translate-y-1 ease-out border-2 border-blue-900 rounded-lg w-40 h-20 bg-blue-400 hover:bg-blue-700 duration-200 font-mono text-2xl antialiased'>
+            Contact
           </button>
       </div>
     </div>
   )
 }
-
-export default App
